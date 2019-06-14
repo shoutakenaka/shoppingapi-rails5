212a92c3993308939ecf703f3f302a58523495ae
Shou Takenaka <sasaminn@gmail.com>
2019-06-14 17:02:59 +0000

    Define carts table schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_165458) do
+ActiveRecord::Schema.define(version: 2019_06_14_170040) do
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
