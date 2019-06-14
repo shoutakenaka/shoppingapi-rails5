6d2d6ea8bac3a33458bf0a20944ea9c1670635fa
Shou Takenaka <sasaminn@gmail.com>
2019-06-14 16:52:59 +0000

    Define orders table schema

--- db/migrate/20190614164745_create_orders.rb
+++ db/migrate/20190614164745_create_orders.rb
@@ -1,7 +1,21 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-
+      t.integer :total
+      t.string :shipping_first_name
+      t.string :shipping_last_name
+      t.string :shipping_phone
+      t.string :shipping_postal_code
+      t.string :shipping_region
+      t.string :shipping_locality
+      t.string :shipping_street_address
+      t.string :billing_first_name
+      t.string :billing_last_name
+      t.string :billing_phone
+      t.string :billing_postal_code
+      t.string :billing_region
+      t.string :billing_locality
+      t.string :billing_street_address
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_164107) do
+ActiveRecord::Schema.define(version: 2019_06_14_164745) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "unit_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "total"
+    t.string "shipping_first_name"
+    t.string "shipping_last_name"
+    t.string "shipping_phone"
+    t.string "shipping_postal_code"
+    t.string "shipping_region"
+    t.string "shipping_locality"
+    t.string "shipping_street_address"
+    t.string "billing_first_name"
+    t.string "billing_last_name"
+    t.string "billing_phone"
+    t.string "billing_postal_code"
+    t.string "billing_region"
+    t.string "billing_locality"
+    t.string "billing_street_address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
