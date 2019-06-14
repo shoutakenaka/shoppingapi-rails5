82e6e8423c0ffd3bcebdd3fe6e18fc9cc1578a97
Shou Takenaka <sasaminn@gmail.com>
2019-06-14 16:47:15 +0000

    Define items table schema

--- db/migrate/20190614164107_create_items.rb
+++ db/migrate/20190614164107_create_items.rb
@@ -1,7 +1,9 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.string :name
+      t.text :description
+      t.integer :unit_price
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_163923) do
+ActiveRecord::Schema.define(version: 2019_06_14_164107) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "unit_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id"
