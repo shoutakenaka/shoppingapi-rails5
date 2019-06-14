4b424239bca3db504a82a695e84c40c15edfd493
Shou Takenaka <sasaminn@gmail.com>
2019-06-14 16:38:50 +0000

    Define users table schema

--- db/migrate/20190614162603_create_users.rb
+++ db/migrate/20190614162603_create_users.rb
@@ -1,7 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string :username
+      t.string :password_digest
+      t.string :email
+      t.string :phone
+      t.string :postal_code
+      t.string :region
+      t.string :locality
+      t.string :street_address
       t.timestamps
     end
   end
