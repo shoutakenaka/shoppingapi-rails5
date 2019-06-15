c9dc3c7d4f65bb5f1c5683b9513e2ebcfb911115
Shou Takenaka <sasaminn@gmail.com>
2019-06-15 15:35:16 +0000

    Allow to sign up

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,17 @@
 class CustomersController < ApplicationController
+  def create
+    Customer.create!(
+      username: params[:username],
+      password: params[:password],
+      password_confirmation: params[:password],
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone: params[:phone],
+      postal_code: params[:postal_code],
+      region: params[:region],
+      locality: params[:locality],
+      street_address: params[:street_address])
+    render json: {}
+  end
 end
