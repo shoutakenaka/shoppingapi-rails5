18e9281bfbf916ed0a13edefde2392c14238011e
Shou Takenaka <sasaminn@gmail.com>
2019-06-16 07:12:29 +0000

    Allow to login as admin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,24 @@ require 'securerandom'
 
 class SessionsController < ApplicationController
   def create
-    user = User.find_by!(username: params[:username])
-    if !user.authenticate(params[:password])
+    load_user
+    if !@user.authenticate(params[:password])
       return render json: { code: 'not_authenticated' }, status: 401
     end
     token = SecureRandom.base64(64)
-    user.sessions.create!(token: token)
-    render json: { token: token, username: user.username }
+    @user.sessions.create!(token: token)
+    render json: { token: token, username: @user.username }
+  end
+  
+  private
+  
+  def load_user
+    if params[:role] == 'admin'
+      @user = Admin.find_by!(username: params[:username])
+    else
+      logger.warn("Unknown role specified (role='#{params[:role]}').") if params[:role].present?
+      @user = Customer.find_by!(username: params[:username])
+    end
+    @user
   end
 end
