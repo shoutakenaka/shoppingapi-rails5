8b6145dbffe020f6778d7e43649e745ab05c29e4
Shou Takenaka <sasaminn@gmail.com>
2019-06-16 07:23:30 +0000

    Ensure to authorize all requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+  before_action :authorize
+
+  private
+
+  def authorize
+    token = request.headers[:AUTHORIZATION]
+    current_session = Session.find_by(token: token)
+    @current_user = current_session&.user
+    if @current_user.blank?
+      return render json: { code: 'invalid_token' }, status: 403
+    end
+  end
+
+  def current_user
+    @current_user
+  end
 end
