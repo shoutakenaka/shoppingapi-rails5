72692883bd6d1e54d012e8a9fa95bebb2ac58529
Shou Takenaka <sasaminn@gmail.com>
2019-06-16 07:46:58 +0000

    Define routes for items controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :items, only: %i(index show create udpate destroy)
   resources :sessions, only: %i(create)
   resources :customers, only: %i(create)
 end
