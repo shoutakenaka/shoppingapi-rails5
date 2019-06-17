79754bcbad49f35aae2abd361cae300d8f67d975
Shou Takenaka <sasaminn@gmail.com>
2019-06-17 16:24:58 +0000

    Allow to associate image

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
   validates :name, presence: true
   validates :unit_price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
