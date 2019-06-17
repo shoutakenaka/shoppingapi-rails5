c598d1e7b2f8ea3441847835ca14f476a090393e
Shou Takenaka <sasaminn@gmail.com>
2019-06-17 15:16:23 +0000

    Use transaction

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,15 +6,17 @@ class CartsController < ApplicationController
   end
 
   def update
-    @cart = current_user.cart || current_user.create_cart(total: 0)
-    @cart.cart_lines.destroy_all
-    items_params = params[:items] || []
-    items_params.each do |o|
-      item = Item.find(o[:id])
-      @cart.cart_liens.create!(item: item, quantity: o[:quantity] || 1)
+    ActiveRecord::Base.transaction do
+      @cart = current_user.cart || current_user.create_cart(total: 0)
+      @cart.cart_lines.destroy_all
+      items_params = params[:items] || []
+      items_params.each do |o|
+        item = Item.find(o[:id])
+        @cart.cart_liens.create!(item: item, quantity: o[:quantity] || 1)
+      end
+      @cart.total = @cart.cart_liens.map { |o| o.quantity * o.item.unit_price }.sum
+      @cart.save!
     end
-    @cart.total = @cart.cart_liens.map { |o| o.quantity * o.item.unit_price }.sum
-    @cart.save!
     render action: :show
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,36 +11,38 @@ class OrdersController < ApplicationController
   end
 
   def create
-    shipping_params = params[:shipping] || {}
-    billing_params = params[:billing] || {}
-    @order = current_uesr.orders.build(
-      status: 1,
-      shipping_first_name: shipping_params[:first_name],
-      shipping_last_name: shipping_params[:last_name],
-      shipping_phone: shipping_params[:phone],
-      shipping_postal_code: shipping_params[:postal_code],
-      shipping_region: shipping_params[:region],
-      shipping_locality: shipping_params[:locality],
-      shipping_street_address: shipping_params[:street_address],
-      billing_first_name: billing_params[:first_name],
-      billing_last_name: billing_params[:last_name],
-      billing_phone: billing_params[:phone],
-      billing_postal_code: billing_params[:postal_code],
-      billing_region: billing_params[:region],
-      billing_locality: billing_params[:locality],
-      billing_street_address: billing_params[:street_address])
-
-    current_user.cart.cart_lines.each do |line|
-      @order.order_lines.build(
-        item: line.item,
-        name: line.item.name,
-        unit_price: line.item.unit_price,
-        quantity: line.quantity,
-        subtotal: item.unit_price * line.quantity)
+    ActiveRecord::Base.transaction do
+      shipping_params = params[:shipping] || {}
+      billing_params = params[:billing] || {}
+      @order = current_uesr.orders.build(
+        status: 1,
+        shipping_first_name: shipping_params[:first_name],
+        shipping_last_name: shipping_params[:last_name],
+        shipping_phone: shipping_params[:phone],
+        shipping_postal_code: shipping_params[:postal_code],
+        shipping_region: shipping_params[:region],
+        shipping_locality: shipping_params[:locality],
+        shipping_street_address: shipping_params[:street_address],
+        billing_first_name: billing_params[:first_name],
+        billing_last_name: billing_params[:last_name],
+        billing_phone: billing_params[:phone],
+        billing_postal_code: billing_params[:postal_code],
+        billing_region: billing_params[:region],
+        billing_locality: billing_params[:locality],
+        billing_street_address: billing_params[:street_address])
+  
+      current_user.cart.cart_lines.each do |line|
+        @order.order_lines.build(
+          item: line.item,
+          name: line.item.name,
+          unit_price: line.item.unit_price,
+          quantity: line.quantity,
+          subtotal: item.unit_price * line.quantity)
+      end
+      
+      @order.total = order.order_lines.map { |o| o.subtotal }.sum
+      @order.save!
     end
-    
-    @order.total = order.order_lines.map { |o| o.subtotal }.sum
-    @order.save!
 
     render action: :show
   end
