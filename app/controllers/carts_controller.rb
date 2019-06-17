eb639e82b6ce82020769c29fde625902d9d9cf0d
Shou Takenaka <sasaminn@gmail.com>
2019-06-17 15:12:53 +0000

    Allow to manage cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,20 @@
 class CartsController < ApplicationController
+  before_action :ensure_user_is_customer
+
+  def show
+    @cart = current_user.cart
+  end
+
+  def update
+    @cart = current_user.cart || current_user.create_cart(total: 0)
+    @cart.cart_lines.destroy_all
+    items_params = params[:items] || []
+    items_params.each do |o|
+      item = Item.find(o[:id])
+      @cart.cart_liens.create!(item: item, quantity: o[:quantity] || 1)
+    end
+    @cart.total = @cart.cart_liens.map { |o| o.quantity * o.item.unit_price }.sum
+    @cart.save!
+    render action: :show
+  end
 end
