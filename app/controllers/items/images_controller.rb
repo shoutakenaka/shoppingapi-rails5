71c2daedd7e260d1c1435b7b5b6b1b8e7ac93494
Shou Takenaka <sasaminn@gmail.com>
2019-06-19 15:49:53 +0000

    Generate items::images controller

---
+++ app/controllers/items/images_controller.rb
@@ -0,0 +1,2 @@
+class Items::ImagesController < ApplicationController
+end
---
+++ test/controllers/items/images_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Items::ImagesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
