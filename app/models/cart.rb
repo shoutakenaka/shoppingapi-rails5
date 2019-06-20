037bd140546abb5a90e0d0f7f25746cbcfdbdde4
Shou Takenaka <sasaminn@gmail.com>
2019-06-20 18:17:45 +0000

    Allow to cascade deletion

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,6 @@
 class Cart < ApplicationRecord
   belongs_to :customer
-  has_many :cart_lines
+  has_many :cart_lines, dependent: :delete_all
   
   validates :customer, presence: true
   validates :total, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
