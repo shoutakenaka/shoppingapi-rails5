5cd8f8459f2a3058116a6f1451c5964d94525533
Shou Takenaka <sasaminn@gmail.com>
2019-06-21 15:15:43 +0000

    Add validations to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,13 @@
 class Customer < User
   has_one :cart
   has_many :orders
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
+  validates :postal_code, presence: true
+  validates :region, presence: true
+  validates :locality, presence: true
+  validates :street_address, presence: true
 end
