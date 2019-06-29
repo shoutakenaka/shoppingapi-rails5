4ac56a9f4cfc1c2da487d4e12cd0efef24ba3293
Shou Takenaka <sasaminn@gmail.com>
2019-06-29 00:26:36 +0000

    Disable authorization

--- app/controllers/items/images_controller.rb
+++ app/controllers/items/images_controller.rb
@@ -1,4 +1,6 @@
 class Items::ImagesController < ApplicationController
+  skip_before_action :authorize
+
   def show
     item = Item.find(params[:item_id])
     render body: item.image.download, content_type: item.image.content_type
