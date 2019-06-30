074ba9f3b24b39e4acadd95eb5e47e4defe9ccb6
Shou Takenaka <sasaminn@gmail.com>
2019-06-30 14:52:47 +0000

    Define browsing_histories table schema

--- db/migrate/20190630144709_create_browsing_histories.rb
+++ db/migrate/20190630144709_create_browsing_histories.rb
@@ -1,7 +1,8 @@
 class CreateBrowsingHistories < ActiveRecord::Migration[5.2]
   def change
     create_table :browsing_histories do |t|
-
+      t.references :user
+      t.references :item
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_153513) do
+ActiveRecord::Schema.define(version: 2019_06_30_144709) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2019_06_17_153513) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "browsing_histories", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_browsing_histories_on_item_id"
+    t.index ["user_id"], name: "index_browsing_histories_on_user_id"
   end
 
   create_table "cart_lines", force: :cascade do |t|
