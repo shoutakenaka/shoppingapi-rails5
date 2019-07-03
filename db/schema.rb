7ca36603e3044c1851368dcaa9e489d872b7a8d4
Shou Takenaka <sasaminn@gmail.com>
2019-07-03 14:43:48 +0000

    Add category_id to items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_01_122627) do
+ActiveRecord::Schema.define(version: 2019_07_03_144213) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2019_07_01_122627) do
     t.integer "unit_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
   end
 
   create_table "order_lines", force: :cascade do |t|
